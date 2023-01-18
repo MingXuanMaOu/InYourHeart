6ceaa7c07ce793c65fb47831c8eae14acb22b1d6
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-18 23:00:34 +0800

    Initial Commit

---
+++ InYourHeart/InYourHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InYourHeartApp.swift
+//  InYourHeart
+//
+//  Created by 刘铭 on 2023/1/18.
+//
+
+import SwiftUI
+
+@main
+struct InYourHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
